0e4bb6c4d2dfbb6492fe156035538628a869d540
Jay Patel <japatel017@gmail.com>
2024-04-25 10:18:06 +0530

    database doneeeeeee

---
+++ messages.sql
@@ -0,0 +1,36 @@
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `msg_id` int(11) NOT NULL,
+  `incoming_msg_id` int(255) NOT NULL,
+  `outgoing_msg_id` int(255) NOT NULL,
+  `msg` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`msg_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- users.sql
+++ users.sql
@@ -43,9 +43,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `unique_id`, `fname`, `lname`, `email`, `password`, `img`, `status`, `code`, `otpstatus`) VALUES
-(23, 1498571052, 'Jay', 'Patel', 'japatel017@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '16828439291681840574H.jpeg', 'Offline now', 0, 'verified'),
-(24, 1454818864, 'Shalin', 'Patel', '21bce215@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '16828440891681840623N.jpeg', 'Offline now', 0, 'verified');
+INSERT INTO `users` (`user_id`, `unique_id`, `fname`, `lname`, `email`, `password`, `img`, `status`, `code`) VALUES
+(23, 1498571052, 'Jay', 'Patel', 'japatel017@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '16828439291681840574H.jpeg', 'Offline now', 0),
+(24, 1454818864, 'Shalin', 'Patel', '21bce215@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', '16828440891681840623N.jpeg', 'Offline now', 0);
 
 --
 -- Indexes for dumped tables
@@ -97,39 +97,3 @@ SET time_zone = "+00:00";
 --
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `messages`
---
-
-CREATE TABLE `messages` (
-  `msg_id` int(11) NOT NULL,
-  `incoming_msg_id` int(255) NOT NULL,
-  `outgoing_msg_id` int(255) NOT NULL,
-  `msg` varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `messages`
---
-ALTER TABLE `messages`
-  ADD PRIMARY KEY (`msg_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `messages`
---
-ALTER TABLE `messages`
-  MODIFY `msg_id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
